0d4638166f21a5fb3a7a06c5fee5e0473c5fcd35
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-04-30 22:21:38 -0300

    validate cpf

--- validadorCPF.py
+++ validadorCPF.py
@@ -2,12 +2,34 @@
 
 resultado = 0
 
-cpf = '13366946458'
-noveDigitos = cpf[:9]
+cpfEnviado = input('digite seu cpf: ')
+noveDigitos = cpfEnviado[:9]
 
-regressar = len(cpf)
+regressar = 10
 for digito in noveDigitos:
-  regressar -= 1
   resultado += int(digito) * int(regressar)
-  print(digito , regressar)
-print(resultado)
+  regressar -= 1
+primeiroDigito = (resultado * 10) % 11
+
+primeiroDigito = primeiroDigito if primeiroDigito <= 9 else 0
+
+
+
+dezDigitos = noveDigitos + str(primeiroDigito)
+regressar2 = 11
+
+resultado2 = 0
+
+for digito in dezDigitos:
+  resultado2 += int(digito) * regressar2
+  regressar2 -= 1
+segundoDigito = (resultado2 * 10) % 11
+segundoDigito= segundoDigito if segundoDigito <= 9 else 0
+
+cpfGerado = f'{noveDigitos}{primeiroDigito}{segundoDigito}'
+
+
+if cpfEnviado == cpfGerado :
+  print(f'{cpfEnviado} é valido')
+else:
+  print('CPF invalido')
