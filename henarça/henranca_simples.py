8d37acf4f1c071149bed4eb677ac7c2be5aa4533
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-05-25 21:35:23 -0300

    herança

--- henarça/henranca_simples.py
+++ henarça/henranca_simples.py
@@ -1,12 +1,23 @@
 class Veiculo:
-  def __init__(self , cor , marca , qtd_pneu , placa ):
+  def __init__(self , cor , marca , qtd_pneu , placa , tipo):
     self.cor = cor
     self.marca = marca
     self.qtd_pneu = qtd_pneu
     self.placa = placa
+    self.tipo = tipo
 
   def ligar(self):
-    print('Ligando Motor')
+    return 'Ligando Motor'
+
+  def buzinar (self , tipo):
+    self.tipo = tipo
+    if tipo == 'moto' :
+
+      return 'bibbiiiiiit'
+    elif tipo == 'carro':
+
+      return 'popoooooom'
+
 
 class Moto(Veiculo):
   pass
@@ -15,6 +26,10 @@ class Carro(Veiculo):
 class Bike(Veiculo):
   pass
 
-moto = Moto("preta" , "yamaha" , 2 , "exd-1234")
-moto.ligar()
+moto = Moto("preta" , "yamaha" , 2 , "exd-1234", "moto")
+print(moto.ligar())
 print(moto.cor , moto.placa)
+carro = Carro("prata" , "honda", 4 , "kbr-0061 " ,"carro")
+
+print(carro.cor , carro.placa)
+print(carro.buzinar("carro"))
