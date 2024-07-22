09e854d7882ed0d82630c5a416e55ba01967f9b7
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-07-22 20:06:36 -0300

    add: new commit

--- POO/desafio_4.py
+++ POO/desafio_4.py
@@ -1,3 +1,33 @@
 '''
 Implemente uma classe chamada “Carro” com atributos para armazenar a marca, o modelo e a velocidade atual do carro. Adicione métodos para acelerar, frear e exibir a velocidade atual.
 '''
+
+class Carro:
+  def __init__(self, marca , modelo , velocidade):
+    self.marca = marca
+    self.modelo = modelo
+    self.velocidade = velocidade
+
+  def ligar_carro(self):
+    print('Ligando o Motor...')
+    motor_ligado = True
+    return motor_ligado
+
+  def desligar_carro(self):
+    print('Desligando o Motor...')
+    motor_ligado = False
+    return motor_ligado
+
+  def acelerar(self, motor_ligado = False):
+    if motor_ligado == True:
+      print('Vruuuuum Vruuuumm')
+      velocidade_atual = self.velocidade
+      return velocidade_atual
+
+    print('motor desligado')
+
+golf = Carro('wolkswagen' , 'golf' , '80km')
+golf.ligar_carro()
+golf.acelerar(True)
+
+print(f'Carro: {golf.modelo}\nMarca: {golf.marca}\nVelocidade: {golf.velocidade}')
--- POO/desafio_5.py
+++ POO/desafio_5.py
@@ -2,3 +2,25 @@
 Crie uma classe chamada “Paciente” que possua atributos para armazenar o nome, a idade e o histórico de consultas de um paciente. Implemente métodos para adicionar uma nova consulta ao histórico e exibir as consultas realizadas.
 
 '''
+
+class Paciente:
+  historico_consultas = []
+  def __init__(self, nome , idade ):
+    self.nome = nome
+    self.idade = idade
+
+  def consultar(self):
+
+    consulta = f'==== Consultando ====\nNome: {self.nome}\nIdade: {self.idade}\n'
+
+    return consulta
+
+paciente_1 = Paciente('jose', 52)
+consulta_historico  = paciente_1.consultar()
+Paciente.historico_consultas.append(consulta_historico)
+paciente_2 = Paciente('Maria', 22)
+consulta_historico  = paciente_2.consultar()
+Paciente.historico_consultas.append(consulta_historico)
+
+for consulta in Paciente.historico_consultas:
+  print(consulta)
