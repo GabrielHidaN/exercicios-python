790797724ff85540090729e908073d6bc7201eeb
GabrielHidaN <victorbielgv7@gmail.com>
2025-01-06 18:52:34 -0300

    chalenger: maior e menor

--- challengers/maior_e_menor.py
+++ challengers/maior_e_menor.py
@@ -2,9 +2,18 @@
 
 Exemplo: Entrada: [3, 7, 1, 9, 2] Saída: Maior: 9, Menor: 1'''
 
-def maior_menor(lista_numeros):
-    for numero in range(len(lista_numeros)):
-        ...
+def maior_menor(lista):
+
+    if not lista:
+        return "A lista está vazia"
+
+    maior = max(lista)
+    menor = min(lista)
+    
+    return f'Maior: {maior}, Menor: {menor}'
+
+        
 
 lista = [-2,22, 1,3,2, 8, 4,5,6,7]
-maior_menor(lista)
+resultado = maior_menor(lista)
+print(resultado)
