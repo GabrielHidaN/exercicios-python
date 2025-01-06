ea7a529649a6dfaa91650318e0c4bee6c6059949
GabrielHidaN <victorbielgv7@gmail.com>
2025-01-06 01:04:04 -0300

    firts commit of 2025

---
+++ challengers/maior_e_menor.py
@@ -0,0 +1,10 @@
+'''Descrição: Implemente uma função que retorne o maior e o menor número de uma lista.
+
+Exemplo: Entrada: [3, 7, 1, 9, 2] Saída: Maior: 9, Menor: 1'''
+
+def maior_menor(lista_numeros):
+    for numero in range(len(lista_numeros)):
+        ...
+
+lista = [-2,22, 1,3,2, 8, 4,5,6,7]
+maior_menor(lista)
